2542000e4896c9cb2046936c4bbc44eb001e29be
akkradet <akkradet7@gmail.com>
2019-10-09 10:38:36 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "======Starting setup squid proxy======"
 yum -y update
 yum install -y squid
 systemctl start squid
@@ -7,3 +8,6 @@ systemctl status squid
 # Copy config from github
 curl -o /etc/squid/squid.conf https://raw.githubusercontent.com/akkradet/HTTP-Proxy/master/squid.conf
 systemctl  restart squid
+firewall-cmd --zone=public --add-service squid
+firewall-cmd --reload
+echo "End."
